11ee9cd1cc9b0f942d81c455080d22f1ba8928d2
ChiElephant <anthony.merino@me.com>
2025-01-16 00:55:48 -0700

    setup basic file structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-export default function Home() {
+import Canvas from "@/canvas";
+import Customizer from "../pages/Customizer";
+import Home from "../pages/Home";
+export default function App() {
   return (
-    <div>
-      <h1 className='head-text'>Virtual Stitch</h1>
-    </div>
+    <main className="app transition-all ease-in">
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
   );
 }
