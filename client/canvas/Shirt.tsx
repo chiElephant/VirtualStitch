b8ac5487cada798dba3b0ffc1db98b95422325b4
ChiElephant <anthony.merino@me.com>
2025-01-16 23:59:16 -0700

    rendering 3d shirt

--- client/canvas/Shirt.tsx
+++ client/canvas/Shirt.tsx
@@ -4,11 +4,12 @@ import { easing } from 'maath';
 import { useSnapshot } from 'valtio';
 import { useFrame } from '@react-three/fiber';
 import { Decal, useGLTF, useTexture } from '@react-three/drei';
+import * as THREE from 'three';
 
 import state from '../store';
 const Shirt = () => {
   const snap = useSnapshot(state);
-  const { nodes, materials } = useGLTF('/shirt_baked.glb');
+  const { nodes, materials } = useGLTF('/icons/shirt_baked.glb');
   const logoTexture = useTexture(snap.logoDecal);
   const fullTexture = useTexture(snap.fullDecal);
 
@@ -16,7 +17,7 @@ const Shirt = () => {
     <group>
       <mesh
         castShadow
-        geometry={nodes.T_Shirt_male.geometry}
+        geometry={(nodes.T_Shirt_male as THREE.Mesh).geometry}
         material={materials.lambert1}
         material-roughness={1}
         dispose={null}></mesh>
