86a7ec5fabc1502b1ebbe89f5e53642848df6e49
ChiElephant <anthony.merino@me.com>
2025-01-17 01:39:38 -0700

    formatted tabs, need to add logic

--- components/Tab.tsx
+++ components/Tab.tsx
@@ -1,5 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { StaticImageData } from 'next/image';
+import Image, { StaticImageData } from 'next/image';
+import { useSnapshot } from 'valtio';
+
+import state from '../store';
 
 type Tab = {
   name: string;
@@ -14,7 +17,25 @@ interface TabProps {
 }
 
 const Tab = ({ tab, handleClick, isFilterTab, isActiveTab }: TabProps) => {
-  return <div>Tab</div>;
+  const snap = useSnapshot(state);
+
+  const activeStyles =
+    isFilterTab && isActiveTab ?
+      { backgroundColor: snap.color, opacity: 0.5 }
+    : { backgroundColor: 'transparent', opacity: 1 };
+
+  return (
+    <div
+      key={tab.name}
+      className={`tab-btn ${isFilterTab ? 'rounded-full glassmorphism' : 'rounded-4'}`}
+      onClick={handleClick}>
+      <Image
+        src={tab.icon}
+        alt={tab.name}
+        className={`${isFilterTab ? 'w-2/3 h-2/3' : 'w-11/12 h-11/12 object-contain'}`}
+      />
+    </div>
+  );
 };
 
 export default Tab;
