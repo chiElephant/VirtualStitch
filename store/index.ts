69a5055dc964ecb6a01afc9c292a5d6ae736ce6d
ChiElephant <anthony.merino@me.com>
2025-01-16 23:59:16 -0700

    rendering 3d shirt

--- store/index.ts
+++ store/index.ts
@@ -1,12 +1,12 @@
-import { proxy } from 'valtio'
+import { proxy } from 'valtio';
 
 const state = proxy({
   intro: true,
   color: '#efbd4e',
   isLogoTexture: true,
   isFullTexture: false,
-  logoDecal: './threejs.png',
-  fullDecal: './threejs.png',
-})
+  logoDecal: './icons/threejs.png',
+  fullDecal: './icons/threejs.png',
+});
 
-export default state
+export default state;
