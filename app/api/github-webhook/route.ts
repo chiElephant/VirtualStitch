46335e94b70319e9ff6a7039d800c2d846dd20fe
Anthony Merino <anthony.merino@me.com>
2025-05-23 17:59:27 -0600

    Testing CI comment

--- app/api/github-webhook/route.ts
+++ app/api/github-webhook/route.ts
@@ -1,4 +1,4 @@
-// THIS IS AN ARBITRARY CI/CD PIPELINE TEST COMMENT TO BE REMOVED WHEN FLOW IS CONFIRMED AS WORKING
+// THIS IS AN ARBITRARY CI/CD PIPELINE TEST COMMENT TO BE REMOVED WHEN FLOW IS WORKING
 import { NextRequest } from 'next/server';
 import { verify } from '@octokit/webhooks-methods';
 import { Octokit } from '@octokit/rest';
